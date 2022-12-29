46d85d5712018de5a3d09e4fc163f379b52e28d3
Gonzalo <gonzaduch@outlook.com>
2022-12-29 17:36:33 -0300

    Se agrega Login y Registro

--- app.js
+++ app.js
@@ -12,3 +12,10 @@ app.listen(3000, () => {
 app.get("/", (req, res) => {
   res.sendFile(path.resolve(__dirname, "views/home.html"));
 });
+
+app.get("/register", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "views/pages/register.html"));
+});
+app.get("/login", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "views/pages/login.html"));
+});
