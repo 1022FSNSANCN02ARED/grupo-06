e61b7fb5330b6d9e77ec57bd00221e31a7953e34
RenzoST <renzosotelo10@gmail.com>
2023-01-25 21:31:50 -0300

    crea ruta de create y la funcion store en el controlador de productos

--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -13,5 +13,25 @@ module.exports = {
         res.render('pages/details', {
             productos
         });
+    },
+    create: (req, res) =>{
+        res.render('pages/create')
+    },
+
+    store: (req, res) =>{
+        const product = {
+            id: Date.now(),
+            name: req.body.name,
+            description: req.body.description,
+            category: req.body.category,
+            price: Number(req.body.price),
+            discount: Number(req.body.discount),
+            image: req.file ? req.file.filename : "default-image.png",
+        };
+        // res.send(product);
+        products.saveProduct(product);
+
+        res.redirect("/products");
     }
+
 }
--- src/routers/productRouter.js
+++ src/routers/productRouter.js
@@ -5,6 +5,8 @@ const router = Router();
 router.get('/', productController.catalogue);
 router.get('/catalogo', productController.catalogue);
 router.get('/carrito', productController.cart);
+router.get('/create', productController.create);
 router.get('/:id', productController.details);
+
 
 module.exports = router;
