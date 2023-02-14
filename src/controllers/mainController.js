5089115dcd3b4aab0d54fc66952ff9140b5e3402
Tomasvii <tomasvillarrealfederco@gmail.com>
2023-02-14 16:30:03 -0300

    hashing register

--- src/controllers/mainController.js
+++ src/controllers/mainController.js
@@ -1,33 +1,30 @@
-const fs = require("fs");
-const path = require("path");
 const jsonDb = require("../data/products");
+const bcryptjs = require("bcryptjs");
 
 const usersModel = jsonDb("usersDataBase");
 
 module.exports = {
-  home: (req, res) => {
-    res.render("home");
-  },
-  login: (req, res) => {
-    res.render("pages/login");
-  },
-  register: (req, res) => {
-    res.render("pages/register");
-  },
-  processRegister:(req, res)=>{
-   
-    if(req.body){
-      let user = req.body;
-      user.avatar = req.file.filename;
-      
-      usersModel.create(user);
-      res.redirect("/");
-    } else {
-      res.render("home")
-    }
-       
-  },
-  turns: (req, res) => {
-    res.render("pages/turns");
-  },
+    home: (req, res) => {
+        res.render("home");
+    },
+    login: (req, res) => {
+        res.render("pages/login");
+    },
+    register: (req, res) => {
+        res.render("pages/register");
+    },
+    processRegister: (req, res) => {
+        if (req.body) {
+            let user = req.body;
+            user.avatar = req.file.filename;
+            (user.password = bcryptjs.hashSync(req.body.password, 10)),
+                usersModel.create(user);
+            res.redirect("/");
+        } else {
+            res.render("home");
+        }
+    },
+    turns: (req, res) => {
+        res.render("pages/turns");
+    },
 };
