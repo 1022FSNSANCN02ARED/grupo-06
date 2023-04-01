9e2bf79b6fa03bcc2ed5caafa62ab4f9baa0c367
RenzoST <renzosotelo10@gmail.com>
2023-03-31 21:00:34 -0300

    crea modelo de users

---
+++ database/models/User.js
@@ -0,0 +1,56 @@
+module.exports = function (sequelize, DataTypes) {
+    const alias = "Users";
+    const cols = {
+        id:{
+            type: DataTypes.STRING,
+            primaryKey: true,
+        },
+        firstName:{
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        lastName:{
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        userName:{
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        email:{
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        password:{
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        cellphone:{
+            type: DataTypes.BIGINT,
+            allowNull: false,
+        },
+        avatar:{
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+
+    };
+    const config = {
+        tableName: "users",
+        timestamps: false,
+    };
+    const users = sequelize.define(alias, cols, config);
+    users.associate = function (models) {
+        users.hasMany(models.turnos, {
+            foreignKey: "user_id",
+            as: "turno",
+        });
+        users.hasMany(models.cart_products, {
+            foreignKey: "user_id",
+            as: "cart_product",
+        })
+    };
+    return users;
+};
+
+
