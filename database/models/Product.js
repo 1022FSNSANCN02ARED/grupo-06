507b2e73047449b91486333987c6604da9baa2d6
Tomasvii <tomasvillarrealfederco@gmail.com>
2023-04-01 22:21:23 -0300

    models fix

--- database/models/Product.js
+++ database/models/Product.js
@@ -1,55 +1,55 @@
 module.exports = function (sequelize, DataTypes) {
-  let alias = "Product";
-  let cols = {
-    id: {
-      type: DataTypes.INTEGER.UNSIGNED,
-      primaryKey: true,
-      autoIncrement: true,
-    },
-    name: {
-      type: DataTypes.STRING(200),
-      allowNull: false,
-    },
-    price: {
-      type: DataTypes.DECIMAL(10, 0).UNSIGNED,
-      allowNull: false,
-    },
-    brand_id: {
-      type: DataTypes.INTEGER.UNSIGNED,
-      allowNull: false,
-    },
-    description: {
-      type: DataTypes.TEXT,
-      allowNull: false,
-    },
-    discount: {
-      type: DataTypes.INTEGER.UNSIGNED,
-      allowNull: false,
-    },
-  };
-  let config = {
-    tableName: "products",
-    timestamps: false,
-  };
-  let Product = sequelize.define(alias, cols, config);
+    let alias = "Product";
+    let cols = {
+        id: {
+            type: DataTypes.INTEGER.UNSIGNED,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        name: {
+            type: DataTypes.STRING(200),
+            allowNull: false,
+        },
+        price: {
+            type: DataTypes.DECIMAL(10, 0).UNSIGNED,
+            allowNull: false,
+        },
+        brand_id: {
+            type: DataTypes.INTEGER.UNSIGNED,
+            allowNull: false,
+        },
+        description: {
+            type: DataTypes.TEXT,
+            allowNull: false,
+        },
+        discount: {
+            type: DataTypes.INTEGER.UNSIGNED,
+            allowNull: false,
+        },
+    };
+    let config = {
+        tableName: "products",
+        timestamps: false,
+    };
+    let Product = sequelize.define(alias, cols, config);
 
-  Product.associate = function (models) {
-    Product.belongsToMany(models.Category, {
-      as: "categories",
-      through: "product_category",
-      foreignKey: "product_id",
-      otherKey: "category_id",
-      timestamps: false,
-    });
-    /*Product.hasMany(models.Brand, {
-      as: "brand",
-      foreignKey: "brand_id",
-    });
-     Product.hasMany(models.Image, {
-      as: "images",
-      foreignKey: "product_id",
-    });*/
-  };
+    Product.associate = function (models) {
+        Product.belongsToMany(models.Category, {
+            as: "categories",
+            through: "product_category",
+            foreignKey: "product_id",
+            otherKey: "category_id",
+            timestamps: false,
+        });
+        Product.hasMany(models.brands, {
+            as: "brand",
+            foreignKey: "brand_id",
+        });
+        Product.hasMany(models.images, {
+            as: "images",
+            foreignKey: "product_id",
+        });
+    };
 
-  return Product;
+    return Product;
 };
