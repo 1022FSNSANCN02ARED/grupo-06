0d9031c16745f830f7125abe5827f5e502452370
Tomasvii <tomasvillarrealfederco@gmail.com>
2023-05-12 19:07:09 -0300

    turn db

--- src/controllers/mainController.js
+++ src/controllers/mainController.js
@@ -172,7 +172,7 @@ module.exports = {
     },
     turnsProcess: (req, res) => {
         if (!res.locals.isLogged) {
-            promp("Debes estar logueado.");
+            alert("Debes estar logueado.");
         }
         // Poner validación logging in antes de enviar form, de preferencia con js para front
 
