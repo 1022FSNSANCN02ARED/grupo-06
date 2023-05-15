a12d30822414f218db3eb597ad6840658cdc322c
Tomasvii <tomasvillarrealfederco@gmail.com>
2023-05-15 20:44:49 -0300

    forms fix

--- haircut/src/components/MovieDetail.jsx
+++ haircut/src/components/MovieDetail.jsx
@@ -1,24 +1,29 @@
 import React from "react";
 
 function MovieDetail({ title, desc, img, url }) {
-  return (
-    <>
-      <div className="text-center">
-        <img
-          className="img-fluid px-3 px-sm-4 mt-3 mb-4"
-          style={{
-            width: "40rem",
-          }}
-          src={img}
-          alt={title}
-        />
-      </div>
-      <p>{desc}</p>
-      <a className="btn btn-danger" target="_blank" rel="noreferrer" href={url}>
-        View movie detail
-      </a>
-    </>
-  );
+    return (
+        <>
+            <div className="text-center">
+                <img
+                    className="img-fluid px-3 px-sm-4 mt-3 mb-4"
+                    style={{
+                        width: "40rem",
+                    }}
+                    src={img}
+                    alt={title}
+                />
+            </div>
+            <p>{desc}</p>
+            <a
+                className="btn btn-danger"
+                target="_blank"
+                rel="noreferrer"
+                href={url}
+            >
+                View product detail
+            </a>
+        </>
+    );
 }
 
 export default MovieDetail;
