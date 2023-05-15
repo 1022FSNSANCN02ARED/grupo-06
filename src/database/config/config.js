a5076c5f1de15692485e420ef034a1f9fbf5fb33
mataguirre <mataguirre.dev@gmail.com>
2023-05-15 12:42:57 -0300

    detalle de producto y catalogo terminado

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -1,7 +1,7 @@
 module.exports = {
     development: {
         username: "root",
-        password: null,
+        password: "5Ii$mx^O2d61^39",
         database: "grupo6",
         host: "127.0.0.1",
         dialect: "mysql",
