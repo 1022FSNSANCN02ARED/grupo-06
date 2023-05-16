44b0b1bb07bebb537e47e3ae606e6b8709bf5eee
RenzoST <renzosotelo10@gmail.com>
2023-05-16 01:27:26 -0300

    cambia la config de la base de datos que fue subida a un host

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -1,9 +1,9 @@
 module.exports = {
     development: {
-        username: "root",
-        password: null,
-        database: "grupo6",
-        host: "127.0.0.1",
+        username: "grupo6-db",
+        password: "grupo06",
+        database: "grupo6-db_g6",
+        host: "mysql-grupo6-db.alwaysdata.net",
         dialect: "mysql",
        
     },
